595d7b0daa802b73b912c1e369298d9377d84625
Hafidzkholifatur <hafidz.kholifatur@gmail.com>
2022-06-19 12:28:07 +0700

    Logout, Ubah Password (Masih Error)

--- application/views/admin/form/form-ganti-password.php
+++ application/views/admin/form/form-ganti-password.php
@@ -1,86 +1,83 @@
 <div class="page-heading">
-                <div class="page-title">
-                    <div class="row">
-                        <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Form Ganti Password</h3>
-                            <p class="text-subtitle text-muted">Form yang bertujuan untuk mengganti password admin</p>
-                        </div>
-                        <div class="col-12 col-md-6 order-md-2 order-first">
-                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Form Ganti Password</li>
-                                </ol>
-                            </nav>
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>Form Ganti Password</h3>
+                <p class="text-subtitle text-muted">Form yang bertujuan untuk mengganti password admin</p>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Form Ganti Password</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Basic Vertical form layout section start -->
+    <section id="basic-vertical-layouts">
+        <div class="row match-height">
+            <div class="col-md-12 col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Ganti Password</h4>
+                    </div>
+
+                    <div class="card-content">
+                        <div class="card-body">
+                            <?php echo $this->session->flashdata('pesan'); ?>
+                            <form class="form form-vertical" action="<?php echo base_url() . 'admin/ganti_password' ?>" method="post">
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Password Lama</label>
+                                                <div class="position-relative">
+                                                    <input type="password" name="pass_lama" class="form-control" id="pass_lama" value="">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-lock"></i>
+                                                    </div>
+                                                </div>
+                                                <?php echo form_error('pass_lama', '<small class="text-danger pl-3">', '</small>'); ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Password Baru</label>
+                                                <div class="position-relative">
+                                                    <input type="password" name="pass_baru" class="form-control" id="pass_baru" value="">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-key"></i>
+                                                    </div>
+                                                </div>
+                                                <?php echo form_error('pass_baru', '<small class="text-danger pl-3">', '</small>'); ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Ketik Ulang Password Baru</label>
+                                                <div class="position-relative">
+                                                    <input type="password" name="ulang_pass_baru" class="form-control" id="ulang_pass_baru" value="">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-key"></i>
+                                                    </div>
+                                                </div>
+                                                <?php echo form_error('ulang_pass_baru', '<small class="text-danger pl-3">', '</small>'); ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 d-flex justify-content-end">
+                                            <button type="submit" class="btn btn-primary me-1 mb-1">Simpan</button>
+                                            <a href="<?php echo base_url() . 'santri/tabel_santri' ?>" class="btn btn-light-secondary me-1 mb-1">Batal</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-                <!-- Basic Vertical form layout section start -->
-                <section id="basic-vertical-layouts">
-                    <div class="row match-height">
-                        <div class="col-md-12 col-12">
-                            <div class="card"> 
-                                <div class="card-header">
-                                    <h4 class="card-title">Ganti Password</h4>
-                                </div>
-
-                                <div class="card-content">
-                                    <div class="card-body">
-                                        <form class="form form-vertical" action="<?php echo base_url().'admin/ganti_password' ?>" method="post">
-                                            <div class="form-body">
-                                                <div class="row">
-                                                    <div class="col-12">
-                                                        <div class="form-group has-icon-left">
-                                                            <label for="first-name-icon">Password Lama</label>
-                                                            <div class="position-relative">
-                                                                <input type="text" name="pass_lama" class="form-control"
-                                                                    id="first-name-icon" value="">
-                                                                <div class="form-control-icon">
-                                                                    <i class="bi bi-lock"></i>
-                                                                </div>
-                                                            </div>
-                                                            <?php echo form_error('pass_lama'); ?>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-12">
-                                                        <div class="form-group has-icon-left">
-                                                            <label for="first-name-icon">Password Baru</label>
-                                                            <div class="position-relative">
-                                                                <input type="text" name="pass_baru" class="form-control"
-                                                                    id="first-name-icon" value="">
-                                                                <div class="form-control-icon">
-                                                                    <i class="bi bi-key"></i>
-                                                                </div>
-                                                            </div>
-                                                            <?php echo form_error('pass_baru'); ?>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-12">
-                                                        <div class="form-group has-icon-left">
-                                                            <label for="first-name-icon">Ketik Ulang Password Baru</label>
-                                                            <div class="position-relative">
-                                                                <input type="text" name="ulang_pass_baru" class="form-control"
-                                                                    id="first-name-icon" value="">
-                                                                <div class="form-control-icon">
-                                                                    <i class="bi bi-key"></i>
-                                                                </div>
-                                                            </div>
-                                                            <?php echo form_error('ulang_pass_baru'); ?>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-12 d-flex justify-content-end">
-                                                        <button type="submit"
-                                                            class="btn btn-primary me-1 mb-1">Simpan</button>
-                                                        <a href="<?php echo base_url().'santri/tabel_santri' ?>" class="btn btn-light-secondary me-1 mb-1">Batal</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <!-- // Basic Vertical form layout section end -->
             </div>
+        </div>
+    </section>
+    <!-- // Basic Vertical form layout section end -->
+</div>
--- application/views/login.php
+++ application/views/login.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Madrasah Diniyah Raport</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo base_url().'assets/css/bootstrap.css' ?>">
-    <link rel="stylesheet" href="<?php echo base_url().'assets/vendors/bootstrap-icons/bootstrap-icons.css' ?>">
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/css/bootstrap.css' ?>">
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/vendors/bootstrap-icons/bootstrap-icons.css' ?>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="<?php echo base_url().'assets/css/app.css' ?>">
-    <link rel="stylesheet" href="<?php echo base_url().'assets/css/pages/auth.css' ?>">
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/css/app.css' ?>">
+    <link rel="stylesheet" href="<?php echo base_url() . 'assets/css/pages/auth.css' ?>">
 </head>
 
 <body>
@@ -26,28 +26,26 @@
                     <h1 class="auth-title">Log in</h1>
                     <p class="auth-subtitle mb-5">Login untuk masuk kedalam dashboard admin</p>
                     <?php
-        if(isset($_GET['pesan'])){
-             if($_GET['pesan'] == "gagal"){
-                echo"<div class='alert alert-danger'>Login gagal! Username dan password salah</div>";
-             }else if($_GET['pesan'] == "logout"){
-                echo"<div class='alert alert-danger'>Anda telah logout</div>";
-             }else if($_GET['pesan'] == "belumlogin"){
-                echo"<div class='alert alert-success'>Silahkan login dulu</div>";
-            }
-        }
-        ?>
-                    <form action="<?php echo base_url().'welcome/login' ?>" method="post">
+                    if (isset($_GET['pesan'])) {
+                        if ($_GET['pesan'] == "gagal") {
+                            echo "<div class='alert alert-danger'>Login gagal! Username dan password salah</div>";
+                        } else if ($_GET['pesan'] == "logout") {
+                            echo "<div class='alert alert-danger'>Anda telah logout</div>";
+                        } else if ($_GET['pesan'] == "belumlogin") {
+                            echo "<div class='alert alert-success'>Silahkan login dulu</div>";
+                        }
+                    }
+                    ?>
+                    <form action="<?php echo base_url() . 'welcome/login' ?>" method="post">
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" name="username" class="form-control form-control-xl"
-                                placeholder="Masukan Username">
+                            <input type="text" name="username" class="form-control form-control-xl" placeholder="Masukan Username">
                             <div class="form-control-icon">
                                 <i class="bi bi-person"></i>
                                 <?php echo form_error('username'); ?>
                             </div>
                         </div>
                         <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="password" name="password" class="form-control form-control-xl"
-                                placeholder="Masukan Password">
+                            <input type="password" name="password" class="form-control form-control-xl" placeholder="Masukan Password">
                             <div class="form-control-icon">
                                 <i class="bi bi-shield-lock"></i>
                                 <?php echo form_error('password'); ?>
