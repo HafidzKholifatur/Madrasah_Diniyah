bb08c4c8580443933a50698b2456d78d1b2de685
Hafidzkholifatur <hafidz.kholifatur@gmail.com>
2022-06-22 16:07:39 +0700

    WIP List Nilai

--- application/views/admin/form/form-tambah-list.php
+++ application/views/admin/form/form-tambah-list.php
@@ -1,58 +1,58 @@
 <div class="page-heading">
-                <div class="page-title">
-                    <div class="row">
-                        <div class="col-12 col-md-6 order-md-1 order-last">
-                            <h3>Form Tambah List Nilai</h3>
-                            <p class="text-subtitle text-muted">Form untuk menambahkan data List Nilai pengajian</p>
-                        </div>
-                        <div class="col-12 col-md-6 order-md-2 order-first">
-                            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Form Tambah List Nilai</li>
-                                </ol>
-                            </nav>
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6 order-md-1 order-last">
+                <h3>Form Tambah List Nilai</h3>
+                <p class="text-subtitle text-muted">Form untuk menambahkan data List Nilai pengajian</p>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Form Tambah List Nilai</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Basic Vertical form layout section start -->
+    <section id="basic-vertical-layouts">
+        <div class="row match-height">
+            <div class="col-md-12 col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Tambah List Nilai</h4>
+                    </div>
+
+                    <div class="card-content">
+                        <div class="card-body">
+                            <form class="form form-vertical" action="<?php echo base_url() . 'santri/aksi_tambah_santri' ?>" method="post">
+                                <div class="form-body">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="form-group has-icon-left">
+                                                <label for="first-name-icon">Nama List Nilai</label>
+                                                <div class="position-relative">
+                                                    <input type="text" name="nama" class="form-control" placeholder="Masukan Nama List Nilai" id="first-name-icon">
+                                                    <div class="form-control-icon">
+                                                        <i class="bi bi-list"></i>
+                                                    </div>
+                                                </div>
+                                                <?php echo form_error('nama'); ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-12 d-flex justify-content-end">
+                                            <button type="submit" class="btn btn-primary me-1 mb-1">Tambah</button>
+                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">Batal</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-                <!-- Basic Vertical form layout section start -->
-                <section id="basic-vertical-layouts">
-                    <div class="row match-height">
-                        <div class="col-md-12 col-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4 class="card-title">Tambah List Nilai</h4>
-                                </div>
-
-                                <div class="card-content">
-                                    <div class="card-body">
-                                        <form class="form form-vertical" action="<?php echo base_url() . 'santri/aksi_tambah_santri' ?>" method="post">
-                                            <div class="form-body">
-                                                <div class="row">
-                                                    <div class="col-12">
-                                                        <div class="form-group has-icon-left">
-                                                            <label for="first-name-icon">Nama List Nilai</label>
-                                                            <div class="position-relative">
-                                                                <input type="text" name="nama" class="form-control" placeholder="Masukan Nama List Nilai" id="first-name-icon">
-                                                                <div class="form-control-icon">
-                                                                    <i class="bi bi-list"></i>
-                                                                </div>
-                                                            </div>
-                                                            <?php echo form_error('nama'); ?>
-                                                        </div>
-                                                    </div>
-                                                    <div class="col-12 d-flex justify-content-end">
-                                                        <button type="submit" class="btn btn-primary me-1 mb-1">Tambah</button>
-                                                        <button type="reset" class="btn btn-light-secondary me-1 mb-1">Batal</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <!-- // Basic Vertical form layout section end -->
             </div>
+        </div>
+    </section>
+    <!-- // Basic Vertical form layout section end -->
+</div>
