900b3603ae5b06f57b93453bdd6406c64f73f02b
Hafidzkholifatur <hafidz.kholifatur@gmail.com>
2022-06-26 14:29:16 +0700

    database update

--- database/db_tpa.sql
+++ database/db_tpa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 20 Jun 2022 pada 16.56
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 7.4.29
+-- Waktu pembuatan: 26 Jun 2022 pada 09.27
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,7 +39,27 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `admin_nama`, `admin_username`, `admin_password`) VALUES
-(1, 'Admin Asw', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+(1, 'Adminin', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `kategori_id` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL,
+  `tgl_dibuat` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`kategori_id`, `nama_kategori`, `tgl_dibuat`) VALUES
+(3, 'dsadas', '2022-06-23'),
+(7, 'UAS', '2022-06-25');
 
 -- --------------------------------------------------------
 
@@ -57,7 +77,9 @@ CREATE TABLE `mapel` (
 --
 
 INSERT INTO `mapel` (`mapel_id`, `mapel_nama`) VALUES
-(2, 'Matematika');
+(2, 'Matematika'),
+(3, 'Aqidah Ahlak'),
+(4, 'Al Qur\'an');
 
 -- --------------------------------------------------------
 
@@ -79,7 +101,8 @@ CREATE TABLE `pengajar` (
 --
 
 INSERT INTO `pengajar` (`pengajar_id`, `pengajar_nama`, `pengajar_jk`, `pengajar_lahir`, `pengajar_telp`, `pengajar_alamat`) VALUES
-(4, 'bu alfi', 'Perempuan', '2022-06-20', '089667190532', 'Griya');
+(5, 'Akmal', 'Laki-Laki', '2022-06-21', '087895', 'jl baru'),
+(6, 'Muhammad Akmal 1', 'Perempuan', '2002-05-23', '0489126', 'Jalanan');
 
 -- --------------------------------------------------------
 
@@ -89,10 +112,18 @@ INSERT INTO `pengajar` (`pengajar_id`, `pengajar_nama`, `pengajar_jk`, `pengajar
 
 CREATE TABLE `penilaian` (
   `penilaian_id` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
   `id_santri` int(11) NOT NULL,
   `id_mapel` int(11) NOT NULL,
   `nilai` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penilaian`
+--
+
+INSERT INTO `penilaian` (`penilaian_id`, `id_kategori`, `id_santri`, `id_mapel`, `nilai`) VALUES
+(5, 3, 6, 2, 1001);
 
 -- --------------------------------------------------------
 
@@ -113,8 +144,8 @@ CREATE TABLE `santri` (
 --
 
 INSERT INTO `santri` (`santri_id`, `santri_nama`, `santri_jk`, `santri_lahir`, `santri_alamat`) VALUES
-(5, 'akmal', 'Laki-Laki', '2022-06-20', 'tanjakan'),
-(6, 'Hafidz', 'Laki-Laki', '2022-06-20', 'kebon');
+(6, 'Hafidz', 'Laki-Laki', '2022-06-20', 'kebon'),
+(8, 'Hani', 'Perempuan', '2022-06-24', 'Jalan');
 
 --
 -- Indexes for dumped tables
@@ -125,6 +156,12 @@ INSERT INTO `santri` (`santri_id`, `santri_nama`, `santri_jk`, `santri_lahir`, `
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`kategori_id`);
 
 --
 -- Indeks untuk tabel `mapel`
@@ -144,7 +181,8 @@ ALTER TABLE `pengajar`
 ALTER TABLE `penilaian`
   ADD PRIMARY KEY (`penilaian_id`),
   ADD KEY `mapel_id` (`id_mapel`),
-  ADD KEY `santri_id` (`id_santri`) USING BTREE;
+  ADD KEY `santri_id` (`id_santri`) USING BTREE,
+  ADD KEY `penilaian_ibfk_3` (`id_kategori`);
 
 --
 -- Indeks untuk tabel `santri`
@@ -163,28 +201,34 @@ ALTER TABLE `admin`
   MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT untuk tabel `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `mapel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `mapel_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengajar`
 --
 ALTER TABLE `pengajar`
-  MODIFY `pengajar_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `pengajar_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `penilaian`
 --
 ALTER TABLE `penilaian`
-  MODIFY `penilaian_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `penilaian_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT untuk tabel `santri`
 --
 ALTER TABLE `santri`
-  MODIFY `santri_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `santri_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -195,7 +239,8 @@ ALTER TABLE `santri`
 --
 ALTER TABLE `penilaian`
   ADD CONSTRAINT `penilaian_ibfk_1` FOREIGN KEY (`id_santri`) REFERENCES `santri` (`santri_id`),
-  ADD CONSTRAINT `penilaian_ibfk_2` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`mapel_id`);
+  ADD CONSTRAINT `penilaian_ibfk_2` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`mapel_id`),
+  ADD CONSTRAINT `penilaian_ibfk_3` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`kategori_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
