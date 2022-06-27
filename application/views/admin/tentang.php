7b7146d1a0ddce1248bb66b6f2f1e9651e01ddcf
Hafidzkholifatur <hafidz.kholifatur@gmail.com>
2022-06-28 00:07:11 +0700

    Final Nilai

--- application/views/admin/tentang.php
+++ application/views/admin/tentang.php
@@ -3,7 +3,7 @@
                     <div class="row">
                         <div class="col-12 col-md-6 order-md-1 order-last">
                             <h3>Tentang</h3>
-                            <p class="text-subtitle text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <p class="text-subtitle text-muted">Berisi informasi tentang Madrasah Diniyah.</p>
                         </div>
                         <div class="col-12 col-md-6 order-md-2 order-first">
                             <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
@@ -21,8 +21,8 @@
                             Simple Datatable
                         </div> -->
                         <div class="card-body">
-                           <h4 class="">Madrasah Diniyah</h4>
-                            <div class="p">Jadi gini, madrasah diniyah adalah sekolah untuk mencari ilmu tentang agama islam.</div>
+                           <h4 class="card-header">Madrasah Diniyah</h4>
+                            <div class="card-body text-wrap"><b>Madrasah Diniyah</b> merupakan sebuah tempat pengajian agama yang beralamat di <b>Jl. Masjid Jami Alfurqon, Kec. Jatiasih, Kota Bks, Jawa Barat 17426.</b> diketuai oleh <b>Bapak Ustadz Nahari Naharudin S.Ag.</b> Sebagai tempat pengajian agama yang terintegrasi dengan Kantor Kementrian Agama, Madrasah Diniyah diwajibkan melakukan penilaian kepada pada Santri/Santriwati. Tujuannya untuk membuat sistem pembelajaran agama yang lebih disiplin dan teratur serta untuk mengembangkan pengetahuan agama Santri/Santriwati dengan Mata Pelajaran tertentu yang nantinya akan dilakukan penilaian. Pembelajaran yang dilakukan akan dibukukan agar Orang Tua dari Santri/Santriwati dapat mengetahui perkembangan anak mereka selama belajar di Tempat Pengajian Agama <b>Madrasah Diniyah.</b></div>
                         </div>
                     </div>
 
